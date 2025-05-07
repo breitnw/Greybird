9f9f06d37dd626fa2afe7d400119ba4e4a8d086a
Nick Breitling <breitling.nw@gmail.com>
2025-05-07 18:45:55 -0500

    "accent"

--- flake.nix
+++ flake.nix
@@ -9,7 +9,28 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = rec {
-          greybird = colorScheme:
+          greybird-with-accent = accent-color:
+            pkgs.stdenvNoCC.mkDerivation {
+              pname = "greybird";
+              version = "3.23.4";
+
+              src = ./.;
+
+              preConfigure = ''
+                sed -i -e 's\#398ee7\#${accent-color}\g' \
+                       ./light/gtk-3.0/_colors.scss
+              '';
+
+              nativeBuildInputs = with pkgs; [ meson ninja pkg-config sassc ];
+
+              buildInputs = with pkgs; [ gdk-pixbuf librsvg ];
+
+              propagatedUserEnvPkgs = with pkgs; [ gtk-engine-murrine ];
+
+              passthru.updateScript = pkgs.gitUpdater { rev-prefix = "v"; };
+            };
+
+          greybird-with-theme = colorScheme:
             pkgs.stdenvNoCC.mkDerivation {
               pname = "greybird";
               version = "3.23.4";
@@ -42,28 +63,7 @@
               passthru.updateScript = pkgs.gitUpdater { rev-prefix = "v"; };
             };
 
-          default = greybird {
-            slug = "tokyo-night-dark";
-            name = "Tokyo Night Dark";
-            palette = {
-              base00 = "1A1B26";
-              base01 = "16161E";
-              base02 = "2F3549";
-              base03 = "444B6A";
-              base04 = "787C99";
-              base05 = "A9B1D6";
-              base06 = "CBCCD1";
-              base07 = "D5D6DB";
-              base08 = "C0CAF5";
-              base09 = "A9B1D6";
-              base0A = "0DB9D7";
-              base0B = "9ECE6A";
-              base0C = "B4F9F8";
-              base0D = "2AC3DE";
-              base0E = "BB9AF7";
-              base0F = "F7768E";
-            };
-          };
+          default = greybird-with-accent "00beef";
         };
       });
 }
